c122b633f107ae27bb84e1de309916c1c1affeac
Sam Young <samkityoung@gmail.com>
2013-02-22 20:35:42 -0500

    Better version.

--- main.rb
+++ main.rb
@@ -3,46 +3,56 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 
+##################################
+#### Initiate ####################
+##################################
 set :sessions, true
 
-###############################
-get '/' do
-	if session[:player_name].nil?
+before do ##ACOUNTED
+	@game_over = false
+end 
+
+##################################
+#### Start and End ###############
+##################################
+get '/' do 
+	if session[:player_name].nil? 
 		redirect '/set_player_names'
 	else
-		redirect '/game'
-	end
-end
+		redirect '/betting'
+	end 
+end 
 
-###############################
-get '/set_player_names' do
+get '/set_player_names' do 
 	erb :set_player_names
-end
+end 
 
-get '/goodbye' do
+get '/goodbye' do 
 	session[:playername] = nil
 	session[:money] = nil
 	erb :goodbye
-end
+end 
 
-post '/set_player_names' do
-	if params['name'].empty?
+post '/set_player_names' do 
+	if params['name'].empty? 
 		@error = "Must input a name, no matter how wonky!"
 		erb :set_player_names
 	else
 		session[:name] = params['name']
 		session[:money] = 1000
 		redirect '/betting'
-	end
-end
+	end 
+end 
 
-###############################
-get '/betting' do
+##################################
+#### Betting #####################
+##################################
+get '/betting' do  
 	erb :betting
-end
+end 
 
-post '/betting' do
-	if params['bet'].to_i == 0
+post '/betting' do 
+	if params['bet'].to_i == 0 
 		@error = "Must input a positive number using only digits 0-9!"
 		erb :betting
 	elsif params['bet'].to_i > session[:money]
@@ -53,31 +63,34 @@ post '/betting' do
 		erb :betting
 	else
 		session[:bet] = params['bet'].to_i
-		redirect '/game'
-	end
-end
+		redirect '/blackjack'
+	end 
+end 
 
-###############################
-get '/game' do
+##################################
+#### Blackjack ###################
+##################################
+get '/blackjack' do 
 
 	#Make sure logged-in
-	if session[:name].empty?
+	if session[:name].empty? 
 		@error = "Must input a name, no matter how wonky!"
 		erb :set_player_names
-	end
+	end 
 
 	#create deck
-	suits = [' of Hearts', ' of Diamonds', ' of Spades', ' of Clubs']
-	face_values = ['2','3','4','5','6','7','8','9','10','Jack','Queen','King','Ace']
+	suits = ['clubs', 'diamonds', 'spades', 'hearts']
+	face_values = ['2','3','4','5','6','7','8','9','10','jack','queen','king','ace']
 	session[:deck] = face_values.product(suits).shuffle
 
 	#deal cards
 	session[:dealer_cards] = []
 	session[:player_cards] = []
-	2.times do
+
+	2.times do 
 		session[:player_cards] << session[:deck].pop
 		session[:dealer_cards] << session[:deck].pop
-	end
+	end 
 
 	#Calculate player totals
 	@player_total = total_calc(session[:player_cards])
@@ -88,55 +101,68 @@ get '/game' do
 
 	#Start flow of game
 
-end
+end 
 
-post '/game' do
-	if params['choice'] == "Hit"
+post '/blackjack/hit_or_stand' do 
+	if params['choice'] == "Hit" 
 		session[:player_cards] << session[:deck].pop
 		@player_total = total_calc(session[:player_cards])
-		if @player_total > 21
+		if @player_total >= 21 
 			@game_over = true
-		end
+		end 
 	elsif params['choice'] == "Stand"
 		@dealer_total = total_calc(session[:dealer_cards])
-		while @dealer_total < 17
+		while @dealer_total < 17 
 			session[:dealer_cards] << session[:deck].pop
 			@dealer_total = total_calc(session[:dealer_cards])
-		end
+		end 
 		@game_over = true
-	elsif params['play_again'] == "Yes"
-		redirect '/betting'
-	else
-		redirect '/goodbye'
-	end
-	
+	end 
+
 	@player_total = total_calc(session[:player_cards])
 	@dealer_total = total_calc(session[:dealer_cards])
 
-	if @game_over == true
-		@outcome = resolve_game(@player_total, @dealer_total)
-	end
+	if @game_over == true 
+		outcome = resolve_game(@player_total, @dealer_total)
+		if outcome[0] == 1
+			@success = outcome[1]
+			@success
+		else
+			@error = outcome[1]
+			@error
+		end
+	end 
 	erb :blackjack
+end 
+
+post '/blackjack/play_again' do
+	if params['play_again'] == "Yes" 
+		redirect '/betting'
+	else
+		redirect '/goodbye'
+	end 
 end
 
-###############################
+##################################
+#### Helper Methods ##############
+##################################
 helpers do
 
 	def total_calc(card_array)
 		total = 0
 		card_array.each do |card|
 			case card[0]
-				when "Ace" then total += 11
-				when "King" then total += 10
-				when "Queen" then total += 10
-				when "Jack" then total += 10
+				when "ace" then total += 11
+				when "king" then total += 10
+				when "queen" then total += 10
+				when "jack" then total += 10
 				else
 					total += card[0].to_i
 			end
 		end
-		total
 
-	    card_array.select{|element| element[0] == "Ace"}.count.times do
+		total
+	    card_array.select{|element| element[0] == "ace"}.count.times do
 	      break if total <= 21
 	      total -= 10
 	    end
@@ -147,26 +173,33 @@ helpers do
 	def resolve_game(player_total,dealer_total)
 		if player_total > 21
 			session[:money] -= session[:bet]
-			"Sorry, you busted! You lose!"
+			[0,"Sorry, you busted! You lose!"]
 		elsif dealer_total > 21
 			session[:money] += session[:bet]
-			"Dealer busted! You win!"
+			[1,"Dealer busted! You win!"]
 		elsif player_total == dealer_total
-			"Tie game! Better than losing, no?"
+			[1,"Tie game! Better than losing, no?"]
+		elsif player_total == 21
+			session[:money] += session[:bet]
+			[1,"You hit blackjack! You win!"]
+		elsif dealer_total == 21
+			session[:money] += session[:bet]
+			[0,"Dealer hit blackjack! You lose."]
 		elsif player_total > dealer_total
 			session[:money] += session[:bet]
-			"Your score beats dealer's score. You win!"
+			[1,"Your score beats dealer's score. You win!"]
 		elsif player_total < dealer_total
 			session[:money] -= session[:bet]
-			"Dealer score beats your score. You lose!"
+			[0,"Dealer score beats your score. You lose!"]
 		else
-			"Something happened. I can't count. Sorry."
+			[0,"Something happened. I can't count. Sorry."]
 		end
 	end
 
-	def has_ace(card_array)
-		card_array[0].each.include? "Ace"
+	def card_image(card)
+		"<img src='/images/cards/#{card[1]}_#{card[0]}.jpg' class='img-polaroid' height='120' width='84'/>"
 	end
+
 
 end
 
